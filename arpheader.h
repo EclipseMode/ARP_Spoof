21027a65bd3cbf855816d4b08d61d3488699d616
EclipseMode <daisy7024@koreatech.ac.kr>
2017-07-30 22:23:15 +0900

    #Fixed ArpHeader

--- arpheader.h
+++ arpheader.h
@@ -12,21 +12,22 @@ typedef unsigned short u_short;
 
 #define __ETHERTYPE_ARP__ 0x0806
 
-typedef struct arpheader{					/* 	Define ARP Header Struct	*/
-	u_int16_t	hw_type;				//[H a r d w a r e T y p e : 2 b y t e s ] 
-	u_int16_t 	proto_type;				//[P r o t o c o l T y p e : 2 b y t e s ]
-	u_int8_t	hw_addr_len;				//[h w a d d r l e n g t h : 2 b y t e s ]
-	u_int8_t	proto_addr_len;				//[p t a d d r l e n g t h : 2 b y t e s ]
-	u_int16_t 	operation;				//[                        o p e r a t i o n : 4 b y t e s                     ]
-	u_int8_t	src_hw_addr;				//[                        s e n d e r m a c : 4 b y t e s                     ]
-	u_int8_t	src_ip_addr;				//[                        s e n d e r i p   : 4 b y t e s                     
-	u_int8_t	dst_hw_addr;				//[                        t a r g e t m a c : 4 b y t e s                     ]
-	u_int8_t	dst_ip_addr;				//[                        t a r g e t i p   : 4 b y t e s                     ]
-}arphdr;
+typedef struct ethheader{
+	uint8_t 	src_hw_addr[6];
+	uint8_t		dst_hw_addr[6];
+	uint16_t	type;
+}ethernet;
 
-typedef struct strarphdr{					/*	Define String Address	*/
-	u_char 		src_hw_addr[18];
-	u_char		src_ip_addr[16];
-}arphdr_str;
+typedef struct arpheader{					/* 	Define ARP Header Struct	*/
+	uint16_t	hw_type;				//[H a r d w a r e T y p e : 2 b y t e s ] 
+	uint16_t 	proto_type;				//[P r o t o c o l T y p e : 2 b y t e s ]
+	uint8_t		hw_addr_len;				//[h w a d d r l e n g t h : 2 b y t e s ]
+	uint8_t		proto_addr_len;				//[p t a d d r l e n g t h : 2 b y t e s ]
+	uint16_t 	operation;				//[                        o p e r a t i o n : 4 b y t e s                     ]
+	uint8_t		src_hw_addr;				//[                        s e n d e r m a c : 4 b y t e s                     ]
+	uint8_t		src_ip_addr;				//[                        s e n d e r i p   : 4 b y t e s                     
+	uint8_t		dst_hw_addr;				//[                        t a r g e t m a c : 4 b y t e s                     ]
+	uint8_t		dst_ip_addr;				//[                        t a r g e t i p   : 4 b y t e s                     ]
+}arphdr;
 
 typedef enum {false, true} bool;				/*	Define Boolean Type	*/
