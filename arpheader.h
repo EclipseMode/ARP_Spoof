8c0bcc89c0c46e4de7278143c22d6397616d3242
EclipseMode <daisy7024@koreatech.ac.kr>
2017-07-30 22:19:17 +0900

    #Fixed ArpHeader

---
+++ arpheader.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <stdint.h>
+
+typedef unsigned char u_char;
+typedef unsigned short u_short;
+
+#define __IPTYPE__ 4
+
+#define __MACADDR_LEN__ 6
+#define __IPADDR_LEN__ 4
+
+#define __ETHERTYPE_ARP__ 0x0806
+
+typedef struct arpheader{					/* 	Define ARP Header Struct	*/
+	u_int16_t	hw_type;				//[H a r d w a r e T y p e : 2 b y t e s ] 
+	u_int16_t 	proto_type;				//[P r o t o c o l T y p e : 2 b y t e s ]
+	u_int8_t	hw_addr_len;				//[h w a d d r l e n g t h : 2 b y t e s ]
+	u_int8_t	proto_addr_len;				//[p t a d d r l e n g t h : 2 b y t e s ]
+	u_int16_t 	operation;				//[                        o p e r a t i o n : 4 b y t e s                     ]
+	u_int8_t	src_hw_addr;				//[                        s e n d e r m a c : 4 b y t e s                     ]
+	u_int8_t	src_ip_addr;				//[                        s e n d e r i p   : 4 b y t e s                     
+	u_int8_t	dst_hw_addr;				//[                        t a r g e t m a c : 4 b y t e s                     ]
+	u_int8_t	dst_ip_addr;				//[                        t a r g e t i p   : 4 b y t e s                     ]
+}arphdr;
+
+typedef struct strarphdr{					/*	Define String Address	*/
+	u_char 		src_hw_addr[18];
+	u_char		src_ip_addr[16];
+}arphdr_str;
+
+typedef enum {false, true} bool;				/*	Define Boolean Type	*/
